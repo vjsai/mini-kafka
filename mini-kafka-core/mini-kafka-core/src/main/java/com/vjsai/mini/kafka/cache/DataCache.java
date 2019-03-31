d9b93e02609a8db9d70c15d9707a16aa7eb1d350
Vijay Sai J N V <vijaysaijnv@gmail.com>
2019-03-31 08:30:54 +0530

    Implementation of POJO's

--- mini-kafka-core/mini-kafka-core/src/main/java/com/vjsai/mini/kafka/cache/DataCache.java
+++ mini-kafka-core/mini-kafka-core/src/main/java/com/vjsai/mini/kafka/cache/DataCache.java
@@ -1,4 +1,70 @@
 package com.vjsai.mini.kafka.cache;
 
-public class DataCache {
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
+
+public class DataCache<K, V> {
+    private final long mDefaultTTL = 15;
+    private long mTTL = 0;
+    private HashMap<K, DataValue<V>> dataMap;
+
+    DataCache() {
+        dataMap = new HashMap<K, DataValue<V>>();
+        mTTL = mDefaultTTL;
+    }
+
+    DataCache(long ttlMinutes) {
+        dataMap = new HashMap<K, DataValue<V>>();
+        mTTL = ttlMinutes;
+    }
+
+    public void put(K key, V value) {
+        dataMap.put(key, new DataValue<V>(value));
+    }
+
+    public V get(K key) {
+        DataValue<V> data = dataMap.get(key);
+        V result = null;
+        if (data != null) {
+            long t_diff = TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis() - data.insertTime);
+            if (t_diff >= mTTL) {
+                dataMap.remove(key);
+                data.value = null;
+            }
+            result = data.value;
+        }
+        return result;
+    }
+
+    public boolean containsKey(K key) {
+        if (dataMap.containsKey(key)) {
+            this.get(key);
+        }
+        return dataMap.containsKey(key);
+    }
+
+
+    public void remove(K key) {
+        if (dataMap.containsKey(key)) {
+            dataMap.remove(key);
+        }
+    }
+
+    public long getmDefaultTTL() {
+        return mDefaultTTL;
+    }
+
+    public long getmTTL() {
+        return mTTL;
+    }
+
+    private final class DataValue<T> {
+        public T value;
+        public long insertTime;
+
+        DataValue(T value) {
+            this.value = value;
+            insertTime = System.currentTimeMillis();
+        }
+    }
 }
--- mini-kafka-core/mini-kafka-core/src/main/java/com/vjsai/mini/kafka/hosts/HostRecord.java
+++ mini-kafka-core/mini-kafka-core/src/main/java/com/vjsai/mini/kafka/hosts/HostRecord.java
@@ -1,4 +1,36 @@
 package com.vjsai.mini.kafka.hosts;
 
-public class HostRecord {
+import java.io.Serializable;
+
+public class HostRecord implements Serializable {
+    private final String host;
+    private final Integer port;
+    public HostRecord(String host, Integer port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public Integer getPort() {
+        return port;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return ((HostRecord) o) != null
+                &&  o instanceof HostRecord
+                && ((HostRecord) o).host.equals(host)
+                && ((HostRecord) o).getPort().equals(port);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 31 * hash + (host == null ? 0 : host.hashCode());
+        hash = 31 * hash + (port == null ? 0 : port.hashCode());
+        return hash;
+    }
 }
