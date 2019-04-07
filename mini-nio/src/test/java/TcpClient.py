dda0087c1cd0b854ee236f13c76c82f84a1cc4ff
Vijay Sai J N V <vijaysaijnv@gmail.com>
2019-04-08 00:25:36 +0530

    updated

--- mini-nio/src/test/java/TcpClient.py
+++ mini-nio/src/test/java/TcpClient.py
@@ -1,9 +1,12 @@
 import socket
+
+count = 0
 while 1:
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(('0.0.0.0', 5000))
-    very_very_big_string = "x" * 1000
+    very_very_big_string = "x" * 100000
     client.sendall(b'' + very_very_big_string)
     data = client.recv(1024)
     client.close()
-    print('Received', repr(data))
+    count = count + 1
+    print('Received '+str(count)+' : ', repr(data))
